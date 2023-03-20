b69dfb9cf768aa58eb3351c49e87abb5f78e2a8d
shekhabdul12 <122611410+shekhabdul12@users.noreply.github.com>
2023-03-20 21:38:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+  JWT_SECRET:
+    'a4cdeb3dfd35bc06cf674626626ac0dba3bcdaf6aa88359d6560c050042ffae0',
+  NAME: 'Javonte Graham',
+  EMAIL: 'javonte40@ethereal.email', // testing email & password
+  PASSWORD: 'UWcRUXJsDdUWmbKGJz',
+  ATLAS_URI:
+    'mongodb+srv://abdulsamad:3B7HSOSrebPdSJUo@cluster0.dgztrkv.mongodb.net/?retryWrites=true&w=majority',
+};
